9619103e2aec1bc60d1eb6039c9fa14c4b9e6afc
julien <j.banchetti@hotmail.fr>
2018-10-25 13:53:24 -0400

    Add layout

--- src/Layout.js
+++ src/Layout.js
@@ -2,6 +2,10 @@
 import { createComponent } from "react-fela";
 
 // Define layout styles
-const Layout = () => ({});
+const Layout = () => ({
+  fontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+  fontSize: "18px",
+  padding: "0 1em"
+});
 
 export default createComponent(Layout);
